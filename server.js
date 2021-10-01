ab3fcca54f8297638f86f7527ac72cebdef5de20
JohanK <klausenjohan23@gmail.com>
2021-10-01 12:41:12 +0200

    added github

--- server.js
+++ server.js
@@ -1,19 +1,12 @@
 'use strict'
 
 const express = require('express');
-const fs = require ('fs');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 8042;
 
-app.get('/', (req, res) => {
-    fs.readFile('index.html', (err, data) => {
-        res.setHeader('Content-Type', 'text/html');
-        res.setHeader('Content-Length', data.length);
-        res.send(data);
-   
-    })
-})
+
 app.get('/index.css', (req, res) => {
     fs.readFile('index.css', (err, data) => {
         res.setHeader('Content-Type', 'text/css');
@@ -21,37 +14,40 @@ app.get('/index.css', (req, res) => {
         res.send(data);
     })
 })
-app.get('/Ice-logo.jpg', (req, res) => {
-    fs.readFile('Ice-logo.jpg', (err, data) => {
+
+app.get('/johan', (req, res) => {
+    fs.readFile('Image/selfie.jpg', (err, data) => {
         res.setHeader('Content-Type', 'image/jpg');
-       
-        res.send(data);
-    })
-})
-app.get('/selfie.jpg', (req, res) => {
-    fs.readFile('selfie.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-       
-        res.send(data);
-    })
-})
-app.get('/vigor1.jpg', (req, res) => {
-    fs.readFile('vigor1.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-       
-        res.send(data);
-    })
-})
-app.get('/lawe.jpg', (req, res) => {
-    fs.readFile('lawe.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-       
+
         res.send(data);
     })
 })
 
 
+app.get('/vigor1', (req, res) => {
+    fs.readFile('Image/vigor1.jpg', (err, data) => {
+        res.setHeader('Content-Type', 'image/jpg');
 
-app.listen(PORT,() => {
+        res.send(data);
+    })
+})
+app.get('/lawe', (req, res) => {
+    fs.readFile('Image/lawe.jpg', (err, data) => {
+        res.setHeader('Content-Type', 'image/jpg');
+
+        res.send(data);
+    })
+})
+app.get('/', (req, res) => {
+    fs.readFile('index.html', (err, data) => {
+        res.setHeader('Content-Type', 'text/html');
+        res.setHeader('Content-Length', data.length);
+        res.send(data);
+
+    })
+})
+
+
+app.listen(PORT, () => {
     console.log(`the app listen to ${PORT}`);
 })
