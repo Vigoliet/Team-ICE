db39cde6d0eb273fccaea0451def1c4364c717b6
JohanK <klausenjohan23@gmail.com>
2021-10-04 11:04:01 +0200

    used express and made the code easier to read

--- server.js
+++ server.js
@@ -2,42 +2,12 @@
 
 const express = require('express');
 const fs = require('fs');
-
 const app = express();
 const PORT = process.env.PORT || 8042;
 
-
-app.get('/index.css', (req, res) => {
-    fs.readFile('index.css', (err, data) => {
-        res.setHeader('Content-Type', 'text/css');
-        res.setHeader('contet-Length', data.length);
-        res.send(data);
-    })
-})
-
-app.get('/johan', (req, res) => {
-    fs.readFile('Image/selfie.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-
-        res.send(data);
-    })
-})
+app.use(express.static('public'));
 
 
-app.get('/vigor1', (req, res) => {
-    fs.readFile('Image/vigor1.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-
-        res.send(data);
-    })
-})
-app.get('/lawe', (req, res) => {
-    fs.readFile('Image/lawe.jpg', (err, data) => {
-        res.setHeader('Content-Type', 'image/jpg');
-
-        res.send(data);
-    })
-})
 app.get('/', (req, res) => {
     fs.readFile('index.html', (err, data) => {
         res.setHeader('Content-Type', 'text/html');
